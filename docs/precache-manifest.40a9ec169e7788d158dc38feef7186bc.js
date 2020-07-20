b598e6cb53fcb6ce74adbfead2ece4a24ae59a4d
JA HA <hiraldo06@gmail.com>
2020-07-20 17:58:52 -0400

    docs add

---
+++ docs/precache-manifest.40a9ec169e7788d158dc38feef7186bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8727a3d62d9ede448c173475ab6d75ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b9a970824ff211613b0",
+    "url": "/static/css/main.45933230.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9a970824ff211613b0",
+    "url": "/static/js/main.7034213b.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
